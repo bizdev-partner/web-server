5c55827a8c6e74c47feae04f16fc70f98c69647e
josh-vannatta <jjvannatta88@gmail.com>
2024-12-09 13:41:14 -0700

    added npm package

---
+++ domain/index.ts
@@ -0,0 +1,8 @@
+export * as Activities from './activity';
+export * as Campaigns from './campaign';
+export * as Clients from './client';
+export * as Common from './common';
+export * as Leads from './lead';
+export * as Reports from './report';
+export * as Sales from './sales_package';
+export * as Workflows from './workflow';
