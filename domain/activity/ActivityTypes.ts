4c8bfbe3fabcbc33676b0f5df4dbdc9fa0709e11
josh-vannatta <jjvannatta88@gmail.com>
2025-01-02 20:11:18 -0700

    filled in all activity outcome -> leadStatus

--- domain/activity/ActivityTypes.ts
+++ domain/activity/ActivityTypes.ts
@@ -12,7 +12,6 @@ export class ActivityType extends Enumeration {
 
     public addOutcomes(outcomes: ActivityOutcome[]) {
         this.outcomes = outcomes;
-
         return this;
     }
 
@@ -20,90 +19,90 @@ export class ActivityType extends Enumeration {
     public static readonly EmailOutreach = new ActivityType(
         { id: 1, name: "Email Outreach" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Email Sent", success: true, leadStatus: LeadStatus.Unqualified }),
-            new ActivityOutcome({ description: "Bounced", success: false }),
-            new ActivityOutcome({ description: "No Response", success: false }),
+            new ActivityOutcome({ description: "Email Sent", success: true, leadStatus: LeadStatus.Pursued }),
+            new ActivityOutcome({ description: "Bounced", success: false, leadStatus: LeadStatus.Lost }),
+            new ActivityOutcome({ description: "No Response", success: false, leadStatus: LeadStatus.Unqualified }),
         ]
     );
 
     public static readonly PhoneCall = new ActivityType(
         { id: 2, name: "Phone Call" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Call Connected", success: true }),
-            new ActivityOutcome({ description: "No Answer", success: false }),
-            new ActivityOutcome({ description: "Wrong Number", success: false }),
+            new ActivityOutcome({ description: "Call Connected", success: true, leadStatus: LeadStatus.Pursued }),
+            new ActivityOutcome({ description: "No Answer", success: false, leadStatus: LeadStatus.Unqualified }),
+            new ActivityOutcome({ description: "Wrong Number", success: false, leadStatus: LeadStatus.Lost }),
         ]
     );
 
     public static readonly InPersonMeeting = new ActivityType(
         { id: 3, name: "In-Person Meeting" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Meeting Held", success: true }),
-            new ActivityOutcome({ description: "Client Cancelled", success: false }),
-            new ActivityOutcome({ description: "No Show", success: false }),
+            new ActivityOutcome({ description: "Meeting Held", success: true, leadStatus: LeadStatus.Pursued }),
+            new ActivityOutcome({ description: "Client Cancelled", success: false, leadStatus: LeadStatus.Lost }),
+            new ActivityOutcome({ description: "No Show", success: false, leadStatus: LeadStatus.Lost }),
         ]
     );
 
     public static readonly ProductDemo = new ActivityType(
         { id: 4, name: "Product Demo" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Demo Completed", success: true }),
-            new ActivityOutcome({ description: "Client Uninterested", success: false }),
-            new ActivityOutcome({ description: "Technical Issues", success: false }),
+            new ActivityOutcome({ description: "Demo Completed", success: true, leadStatus: LeadStatus.Pursued }),
+            new ActivityOutcome({ description: "Client Uninterested", success: false, leadStatus: LeadStatus.Unqualified }),
+            new ActivityOutcome({ description: "Technical Issues", success: false, leadStatus: LeadStatus.Lost }),
         ]
     );
 
     public static readonly NetworkingEvent = new ActivityType(
         { id: 5, name: "Networking Event" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Connections Made", success: true }),
-            new ActivityOutcome({ description: "Low Engagement", success: false }),
-            new ActivityOutcome({ description: "Event Cancelled", success: false }),
+            new ActivityOutcome({ description: "Connections Made", success: true, leadStatus: LeadStatus.Pursued }),
+            new ActivityOutcome({ description: "Low Engagement", success: false, leadStatus: LeadStatus.Unqualified }),
+            new ActivityOutcome({ description: "Event Cancelled", success: false, leadStatus: LeadStatus.Lost }),
         ]
     );
 
     public static readonly SocialMediaEngagement = new ActivityType(
         { id: 6, name: "Social Media Engagement" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Post Liked", success: true }),
-            new ActivityOutcome({ description: "Post Ignored", success: false }),
-            new ActivityOutcome({ description: "Negative Feedback", success: false }),
+            new ActivityOutcome({ description: "Post Liked", success: true, leadStatus: LeadStatus.Pursued }),
+            new ActivityOutcome({ description: "Post Ignored", success: false, leadStatus: LeadStatus.Unqualified }),
+            new ActivityOutcome({ description: "Negative Feedback", success: false, leadStatus: LeadStatus.Lost }),
         ]
     );
 
     public static readonly ProposalSubmission = new ActivityType(
         { id: 7, name: "Proposal Submission" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Proposal Accepted", success: true }),
-            new ActivityOutcome({ description: "Proposal Rejected", success: false }),
-            new ActivityOutcome({ description: "Awaiting Response", success: false }),
+            new ActivityOutcome({ description: "Proposal Accepted", success: true, leadStatus: LeadStatus.Won }),
+            new ActivityOutcome({ description: "Proposal Rejected", success: false, leadStatus: LeadStatus.Lost }),
+            new ActivityOutcome({ description: "Awaiting Response", success: false, leadStatus: LeadStatus.Pursued }),
         ]
     );
 
     public static readonly FollowUp = new ActivityType(
         { id: 8, name: "Follow-Up" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Follow-Up Successful", success: true }),
-            new ActivityOutcome({ description: "No Response", success: false }),
-            new ActivityOutcome({ description: "Client Uninterested", success: false }),
+            new ActivityOutcome({ description: "Follow-Up Successful", success: true, leadStatus: LeadStatus.Pursued }),
+            new ActivityOutcome({ description: "No Response", success: false, leadStatus: LeadStatus.Unqualified }),
+            new ActivityOutcome({ description: "Client Uninterested", success: false, leadStatus: LeadStatus.Lost }),
         ]
     );
 
     public static readonly ContractNegotiation = new ActivityType(
         { id: 9, name: "Contract Negotiation" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Contract Finalized", success: true }),
-            new ActivityOutcome({ description: "Negotiations Failed", success: false }),
-            new ActivityOutcome({ description: "Client Requested Revisions", success: false }),
+            new ActivityOutcome({ description: "Contract Finalized", success: true, leadStatus: LeadStatus.Won }),
+            new ActivityOutcome({ description: "Negotiations Failed", success: false, leadStatus: LeadStatus.Lost }),
+            new ActivityOutcome({ description: "Client Requested Revisions", success: false, leadStatus: LeadStatus.Pursued }),
         ]
     );
 
     public static readonly Closing = new ActivityType(
         { id: 10, name: "Closing" }).addOutcomes(
         [
-            new ActivityOutcome({ description: "Deal Closed", success: true }),
-            new ActivityOutcome({ description: "Deal Fell Through", success: false }),
-            new ActivityOutcome({ description: "Postponed", success: false }),
+            new ActivityOutcome({ description: "Deal Closed", success: true, leadStatus: LeadStatus.Won }),
+            new ActivityOutcome({ description: "Deal Fell Through", success: false, leadStatus: LeadStatus.Lost }),
+            new ActivityOutcome({ description: "Postponed", success: false, leadStatus: LeadStatus.Pursued }),
         ]
     );
 
@@ -111,7 +110,6 @@ export class ActivityType extends Enumeration {
         return EnumUtils.fromName(ActivityType, name);
     }
 }
-
 
 export class PriorityType extends Enumeration {
     public static High = new PriorityType({ id: 1, name: "High" });
